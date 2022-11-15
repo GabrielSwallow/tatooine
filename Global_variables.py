33a21386089c3c4d951d7caa8d7a18e2d9d4ba7e
GabrielSwallow <gabrielswallow@googlemail.com>
2022-11-15 17:50:52 +0000

    added more variables to global variables

--- Global_variables.py
+++ Global_variables.py
@@ -5,3 +5,11 @@ Rmax = 70
 fs = 14 # font size
 C0, C1, C2 = 'k', 'b', 'y'
 size = size*a_bin
+
+cart = False # 'cart grid'
+logsc = False # log scale
+nbody = True # nbody integrater used
+nts   = 100 # output time step
+var   = 'rho' # 'rho, vx1, vx2, prs'
+
+name = 'Kep-47' #name of the simulation
