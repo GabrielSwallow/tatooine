fdd045221d789f7cd88a5373f2ceca9e834ebe68
GabrielSwallow <gabrielswallow@googlemail.com>
2022-11-16 16:22:08 +0000

    moved plotters to be a global variable to import into tests

--- analyse.py
+++ analyse.py
@@ -17,15 +17,20 @@ import TwoD_sigma
 import OneD_profile
 import Nbody_Characteristics
 
+plotters = [
+    TwoD_sigma.plot_one, 
+    TwoD_sigma.plot_many, 
+    TwoD_sigma.animate,
+    OneD_profile.plot_one, 
+    OneD_profile.plot_many, 
+    OneD_profile.animate,
+    Nbody_Characteristics.plot_one,
+]
+
 if __name__ == '__main__':
-    plotters = [
-        TwoD_sigma.plot_one, 
-        TwoD_sigma.plot_many, 
-        TwoD_sigma.animate,
-        OneD_profile.plot_one, 
-        OneD_profile.plot_many, 
-        OneD_profile.animate,
-        Nbody_Characteristics.plot_one,
-        ]
-    func_index = UI_helper.selectFunctionsToRun(plotters)
-    eval('{}.{}()'.format(plotters[func_index].__module__, plotters[func_index].__name__))
+    ui_input = UI_helper.selectFunctionsToRun(plotters)
+    if type(ui_input) == int:
+        eval('{}.{}()'.format(plotters[ui_input].__module__, plotters[ui_input].__name__))
+    elif ui_input == 'all':
+        for f in plotters:
+            eval('{}.{}()'.format(f.__module__, f.__name__))
