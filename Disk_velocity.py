dde42e183384534dbbb616664be532c933f55219
GabrielSwallow <gabrielswallow@googlemail.com>
2022-11-22 17:54:49 +0000

    animate disk velocity distribution

--- Disk_velocity.py
+++ Disk_velocity.py
@@ -43,8 +43,45 @@ def plot(data_name: str, data_file_to_plot: int) -> None:
     plt.savefig(save_path)
     plt.close()
 
+def animate() -> None:
+    data_name = UI_helper.selectDataToPlot()
+    out_dir = all_data_dir + data_name + '/out'
+    n_max = Navigation_helper.findMaxFileName(out_dir)
+
+    data_parent_dir = all_data_dir + data_name
+    out_dir = all_data_dir + data_name + '/out' 
+    plots_dir = data_parent_dir + '/Plots/'
+
+    max_file = Navigation_helper.findMaxFileName(out_dir)
+
+    fig = plt.figure()
+    camera = Camera(fig)
+
+    for n in range(max_file+1):
+        data = pluto.Pluto(out_dir)
+        vx1 = data.primitive_variable('vx1', n)[0,:,:] #* data.units['density']
+        print(np.argwhere(np.isnan(vx1)))
+        #sigma[np.isnan(sigma)] = 1.0
+        R, Phi = np.meshgrid(data.grid['faces'][0], data.grid['faces'][1])
+        # sig_r = data.user_def_parameters['SIGMA_REF']
+        # alpha_sigma = data.user_def_parameters['ALPHA_SIGMA']
+
+        plt.plot(R[1,:-1], np.mean(vx1, axis=0), color='orange')
+        plt.xlabel(r'radius [a_bin]')
+        plt.ylabel(r'$ Vx1 \, [g/cm^2]$')
+        plt.title('Kep 47')
+        camera.snap()
+
+    save_path = '{}{}_vx1_profile_{}.gif'.format(plots_dir, data_name, n)
+    repeated_plots = 1
+    while(os.path.isfile(save_path)):
+        save_path = '{}{}_vx1_profile_{}({}).gif'.format(plots_dir, data_name, n, repeated_plots)
+        repeated_plots += 1
+    animation = camera.animate()
+    animation.save(save_path)
+
 
 if __name__ == '__main__':
-    plotters = [plot_one]
+    plotters = [plot_one, animate]
     func_index = UI_helper.selectFunctionsToRun(plotters)
     eval('{}()'.format(plotters[func_index].__name__))
