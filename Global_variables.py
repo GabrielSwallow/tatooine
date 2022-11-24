e7df5856d1a13e805ca5ff8c719ea3aa37c47482
GabrielSwallow <gabrielswallow@googlemail.com>
2022-11-24 17:07:51 +0000

    changed order of Global Variables for UI

--- Global_variables.py
+++ Global_variables.py
@@ -1,3 +1,9 @@
+cart = False
+logsc = False
+nbody = True
+var   = "vx1"
+
+nts   = 100 # output time step
 all_data_dir = '../Data/'
 a_bin = 1
 size = 7.5
@@ -6,10 +12,6 @@ fs = 14 # font size
 C0, C1, C2 = 'k', 'b', 'y'
 size = size*a_bin
 
-cart = False # 'cart grid'
-logsc = False # log scale
-nbody = True # nbody integrater used
-nts   = 100 # output time step
-var   = 'rho' # 'rho, vx1, vx2, prs'
+
 
 name = 'Kep-47' #name of the simulation
