cb99125cefe8e46649245d0126c43cb33b18d8b4
GabrielSwallow <gabrielswallow@googlemail.com>
2022-11-30 22:03:25 +0000

    planet mass parser

--- Data_parser_helper.py
+++ Data_parser_helper.py
@@ -1,8 +1,17 @@
 import re
 import numpy as np
 import UI_helper
+import Navigation_helper
 
-def getNbodyInformation_out(out_dir: str, obj: int):
+def get_planet_mass(data_name: str, obj: int):
+    directories = Navigation_helper.Directories(data_name)
+    with open(directories.planet_ini) as f:
+        lines = f.readlines()
+        nbodies = lines[22:]
+        masses = [float(n[0]) for n in nbodies]
+    return masses
+    
+def getNbodyInformation_out(data_name: str, obj: int):
     '''
     returns 
     (
@@ -13,10 +22,9 @@ def getNbodyInformation_out(out_dir: str, obj: int):
         anomoly,
     )
     '''
-    pluto_log_filename = out_dir + '/pluto.log'
-    nbody_elements_filename = out_dir + '/nbody_orbital_elements.out'
-    
-    dbl_txt = open(pluto_log_filename)
+    directories = Navigation_helper.Directories(data_name)
+
+    dbl_txt = open(directories.pluto_log_filename)
     dbl_strs = dbl_txt.readlines()
     dbl_str = dbl_strs[79]
     dbl_list = list(map(int, re.findall('\d+', dbl_str)))
@@ -30,15 +38,15 @@ def getNbodyInformation_out(out_dir: str, obj: int):
         unfiltered_e,
         unfiltered_omega,
         unfiltered_anomaly,
-    ) = np.loadtxt(nbody_elements_filename, usecols=(0,1,2,3,6,7), unpack = True)
+    ) = np.loadtxt(directories.nbody_elements_filename, usecols=(0,1,2,3,6,7), unpack = True)
 
     time = dbl_time * unfiltered_time[object_id == obj]
 
     if len(time) == 0: 
-        max_object_id = findNumBodies(out_dir) - 1
+        max_object_id = findNumBodies(directories.out_dir) - 1
         print('\nMax object_id = {}. You selected {}. Try again'.format(max_object_id, obj))
         obj, obj_des = UI_helper.selectObjectToPlot()
-        getNbodyInformation_out(out_dir, obj)
+        getNbodyInformation_out(directories.out_dir, obj)
         return
     
     return (
@@ -49,42 +57,43 @@ def getNbodyInformation_out(out_dir: str, obj: int):
         unfiltered_anomaly[object_id == obj]
     )
 
-def getNbodyInformation_dat(out_dir: str, obj: int):
+def getNbodyInformation_dat(data_name: str, obj: int):
     '''
     returns 
     (
         time (T_bin),
         a (a_bin),
         e,
-        period
+        anomoly,
     )
     '''
-    nbody_elements_data_filename = out_dir + '/nbody_orbital_elements.dat'
+    directories = Navigation_helper.Directories()
 
     (
         object_id,
         unfiltered_time,
         unfiltered_a,
         unfiltered_e,
-        unfiltered_period,
-    ) = np.loadtxt(nbody_elements_data_filename, skiprows=12, usecols=(0,1,2,3,8), unpack = True)
+        unfiltered_anomoly,
+    ) = np.loadtxt(directories.nbody_elements_data_filename, skiprows=12, usecols=(0,1,2,3,7), unpack = True)
 
     # TODO: make this 10 be the number of out measurements per orbit
     time = unfiltered_time[object_id == obj] / (2*np.pi)
 
     if len(time) == 0: 
-        max_object_id = findNumBodies(out_dir)
+        max_object_id = findNumBodies(directories.out_dir)
         print('\nMax object_id = {}. You selected {}. Try again'.format(max_object_id, obj))
-        getNbodyInformation_dat(out_dir, obj)
+        getNbodyInformation_dat(directories.out_dir, obj)
         return
     
     return (
         time,
         unfiltered_a[object_id == obj],
         unfiltered_e[object_id == obj],
-        unfiltered_period[object_id == obj],
+        unfiltered_anomoly[object_id == obj],
     )
 
-def findNumBodies(out_dir: str) -> int:
-    _, pid, _, _ = np.loadtxt('{}/nbody.out'.format(out_dir), usecols=(0,1,3,4), unpack=True)
+def findNumBodies(data_name: str) -> int:
+    directories = Navigation_helper.Directories(data_name)
+    _, pid, _, _ = np.loadtxt('{}/nbody.out'.format(directories.out_dir), usecols=(0,1,3,4), unpack=True)
     return len(np.unique(pid))
