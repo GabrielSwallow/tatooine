1e48254eee1c80779acc406bde16ede3589b5f32
GabrielSwallow <gabrielswallow@googlemail.com>
2023-01-21 20:55:28 +0000

    updated test_plotters

--- test_plotters.py
+++ test_plotters.py
@@ -20,16 +20,37 @@ from Global_variables import *
 import TwoD_sigma
 import OneD_profile
 import Nbody_Characteristics
-from analyse import plotters
+import Accretion
+import migration
+import Torque
 
 from unittest.mock import patch, create_autospec
 
+auto_plotters = [
+    # TwoD_sigma.plot_one, 
+    TwoD_sigma.plot_many, 
+    # TwoD_sigma.animate,
+    # OneD_profile.plot_one, 
+    OneD_profile.plot_many, 
+    # OneD_profile.animate,
+    # Nbody_Characteristics.plot_one_using_dat,
+    Nbody_Characteristics.plot_one_using_out,
+    Accretion.plot_one_disk_accretion,
+    Accretion.plot_one_planet_accretion,
+    migration.calculate_migration,
+    Torque.plot_torque,
+]
+
+data_name = UI_helper.selectDataToPlot()
+directories = Navigation_helper.Directories(data_name)
+n_max = Navigation_helper.findMaxFileNumber(directories.out_dir)
 
 @patch('UI_helper.selectFunctionsToRun')
 @patch('UI_helper.selectObjectToPlot')
 @patch('UI_helper.selectManyDataFilesToPlot')
 @patch('UI_helper.selectDataFileToPlot')
 @patch('UI_helper.selectDataToPlot')
+
 def test(
     mock_selectDataToPlot,
     mock_selectDataFileToPlot,
@@ -37,15 +58,22 @@ def test(
     mock_selectObjectToPlot,
     mock_selectFunctionsToRun,
     ):
+    mock_selectDataToPlot.return_value = data_name
 
-    mock_selectDataToPlot.return_value = '1000_Orbits_full'
     mock_selectDataFileToPlot.return_value = 0
-    mock_selectManyDataFilesToPlot.return_value = [0,1,2]
-    mock_selectObjectToPlot.return_value = 2
-    mock_selectFunctionsToRun.return_value = 'all'
+    mock_selectManyDataFilesToPlot.return_value = [0,1, int(n_max/2), int(n_max/2)+1, n_max-1, n_max]
+    mock_selectObjectToPlot.return_value = (2, 'b')
+    # mock_selectFunctionsToRun.return_value = 'all'
 
-    for f in plotters:
-        eval('{}.{}()'.format(f.__module__, f.__name__))
+    failed_plots = []
+    for f in auto_plotters:
+        try:
+            eval('{}.{}()'.format(f.__module__, f.__name__))
+        except:
+            failed_plots.append('{}.{}'.format(f.__module__, f.__name__))
+    print('failed plotters:')
+    for f in failed_plots:
+        print(f)
 
 if __name__ == '__main__':
     test()
