ff43681b4304bcca8d3e28d5a4b223a07a999776
GabrielSwallow <gabrielswallow@googlemail.com>
2023-03-21 21:03:42 +0000

    generalised fns so they cavity can be plotted elsewhere

--- Disc_Characteristics.py
+++ Disc_Characteristics.py
@@ -14,11 +14,12 @@ import plotter_helper
 import Gap_Finder as gap
 
 def plot_disc_e_avg() -> None:
+    plot_params.square()
     data_name = UI_helper.selectDataToPlot()
     directories = Navigation_helper.Directories(data_name)
 
-    fig = plt.figure()
-    plot_the_data_disc_e_avg(fig, data_name)
+    fig, axs = plt.subplots(1,1)
+    plot_the_data_disc_e_avg(axs, data_name)
     fig.tight_layout()
 
     save_path = '{}disc_eccentricity_averages.png'.format(directories.plots_dir)
@@ -32,6 +33,7 @@ def plot_disc_e_avg() -> None:
     plt.close(fig)
 
 def plot_one_multiple_data_sets_overlayed_disc_e_avg() -> None:
+    plot_params.square()
     data_ids = UI_helper.select_many_data_ids_to_overlay(choose_out_file = False)
     plotter_helper.plot_multiple_data_sets_overlayed(data_ids, plot_the_data_disc_e_avg)
 
@@ -56,8 +58,7 @@ def plot_one_multiple_data_sets_overlayed_disc_e_avg() -> None:
 #     plt.savefig(save_path)
 #     plt.close()
 
-def plot_the_data_disc_e_avg(fig: plt.Figure, data_name: str, legend_name: str = '') -> None:
-    plot_params.square()
+def plot_the_data_disc_e_avg(ax: plt.Axes, data_name: str, legend_name: str = '') -> None:
     directories = Navigation_helper.Directories(data_name)
     n_min, n_max = UI_helper.selectPlottingRange(directories.out_dir)
     t_min = n_min * nts
@@ -65,16 +66,25 @@ def plot_the_data_disc_e_avg(fig: plt.Figure, data_name: str, legend_name: str =
 
     (time, _, e, _, _, _, _, _, _, _) = Data_parser_helper.get_averages_data(data_name)
     i_min, i_max = tools.time_split(time, t_min, t_max)
-    plt.plot(time[i_min:i_max], e[i_min:i_max], label = f'{legend_name} gap eccentricity')
+    ax.plot(time[i_min:i_max], e[i_min:i_max], label = f'{legend_name} gap eccentricity')
+
+
 
 
 def plot_gap_parameters_out() -> None:
+    plot_params.square()
     data_name = UI_helper.selectDataToPlot()
     directories = Navigation_helper.Directories(data_name)
     fig, axs = plt.subplots(2,1, sharex = 'all')
-    n_min, n_max = plot_the_data_gap_parameters_out(fig, axs, data_name)
+
+    n_min, n_max = UI_helper.selectPlottingRange(directories.out_dir)
+    avg_num = UI_helper.select_averaging_length()
+    plot_the_data_gap_parameters_out(axs[0], data_name, n_min, n_max, avg_num, data_to_plot='semi major axis')
+    plot_the_data_gap_parameters_out(axs[1], data_name, n_min, n_max, avg_num, data_to_plot='eccentricity')
+    axs[1].set(xlabel = 'Time [$\mathrm{T_{bin}}$]')
 
     fig.tight_layout()
+    # fig.suptitle('Disc Parameters of Kepler-47')
 
     save_path = '{}gap_eccentricity_{}-{}.png'.format(directories.plots_dir, n_min, n_max)
     repeated_plots = 0
@@ -90,11 +100,16 @@ def plot_one_multiple_data_sets_gap_parameters_out() -> None:
     data_ids = UI_helper.select_many_data_ids_to_overlay(choose_out_file = False)
     plotter_helper.plot_multiple_data_sets_overlayed_subplots(data_ids, plot_the_data_gap_parameters_out)
 
-def plot_the_data_gap_parameters_out(fig: plt.Figure, axs: plt.Axes, data_name: str, legend_name: str = '') -> tuple[int]:
+def plot_the_data_gap_parameters_out(
+        ax: plt.Axes, 
+        data_name: str, 
+        n_min: int,
+        n_max: int,
+        avg_num: int,
+        legend_name: str = '', 
+        data_to_plot = 'eccentricity') -> tuple[int]:
     directories = Navigation_helper.Directories(data_name)
-    n_min, n_max = UI_helper.selectPlottingRange(directories.out_dir)
-    avg_num = UI_helper.select_averaging_length()
-
+    
     e = []
     a = []
     t = []
@@ -107,16 +122,17 @@ def plot_the_data_gap_parameters_out(fig: plt.Figure, axs: plt.Axes, data_name: 
         e.append(abs(e_n))
         a.append(abs(a_n))
     
-    axs[0].plot(t[:-avg_num], tools.rolling_average(a, avg_num), label=legend_name)
-    axs[0].set_title('Semi-Major Axis [$\mathrm{a_{bin}}$]')
-
-    axs[1].plot(t[:-avg_num], tools.rolling_average(e, avg_num), label=legend_name)
-    axs[1].set_title('Eccentricity [$\mathrm{e}$]')
-
-    axs[1].set(xlabel = 'Time [$\mathrm{T_{bin}}$]')
-    fig.suptitle('Disc Parameters of Kepler-47')
+    if data_to_plot == 'eccentricity':
+        rolling_avg_e, rolling_avg_time = tools.rolling_average(avg_num, e, t)
+        ax.plot(rolling_avg_time, rolling_avg_e, label=legend_name)
+        ax.set_ylabel('Eccentricity [$\mathrm{e}$]')
+    elif data_to_plot == 'semi major axis':
+        rolling_avg_a, rolling_avg_time = tools.rolling_average(avg_num, a, t)
+        ax.plot(rolling_avg_time, rolling_avg_a, label=legend_name)
+        ax.set_xlabel('Semi-Major Axis [$\mathrm{a_{bin}}$]')
 
     return n_min, n_max
+
 
 if __name__ == '__main__':
     plotters = [
