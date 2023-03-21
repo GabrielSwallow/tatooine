863cf34323c73dba05e438c26e17460806c5df57
GabrielSwallow <gabrielswallow@googlemail.com>
2023-03-21 21:05:27 +0000

    minor refactor, made explicit astrophysical objects, not just a list

--- Global_variables.py
+++ Global_variables.py
@@ -14,22 +14,40 @@ size = size*a_bin
 
 name = 'Kep-47' #name of the simulation
 
-class astrophysical_object():
-    def __init__(self, id: int, name: str, shorthand_name: str) -> None:
-        self.id = id
-        self.name = name
-        self.shorthand_name = shorthand_name
-
 class data_id():
     def __init__(self, name: str, out_file: int, legend_name: str) -> None:
         self.name = name
         self.out_file = out_file
         self.legend_name = legend_name
 
+class astrophysical_object():
+    def __init__(self, id: int, name: str, shorthand_name: str, radius: float) -> None:
+        self.id = id
+        self.name = name
+        self.shorthand_name = shorthand_name
+        self.radius = radius
+
+# class astrophysical_objects():
+#     def __init__(self) -> None:
+#         self.Star1 = astrophysical_object(0, 'Star 1', '1', 1.),
+#         self.Star2 = astrophysical_object(1, 'Star 2', '2', 1.),
+#         self.kep47b = astrophysical_object(2, 'kep47b', '47b', 3.5322),
+#         self.kep47c = astrophysical_object(3, 'kep47c', '47d', 8.5844),
+#         self.kep47d = astrophysical_object(4, 'kep47d', '47c', 11.8330),
+#         self.cavity = astrophysical_object(5, 'Cavity', 'gap', 0)
+
+KepStar1_astrophysical_object = astrophysical_object(0, 'Star 1', '1', 1.)
+KepStar2_astrophysical_object = astrophysical_object(1, 'Star 2', '2', 1.)
+Kep47b_astrophysical_object = astrophysical_object(2, 'kep47b', '47b', 3.53)
+Kep47c_astrophysical_object = astrophysical_object(3, 'kep47c', '47d', 8.5844)
+Kep47d_astrophysical_object = astrophysical_object(4, 'kep47d', '47c', 11.8330)
+cavity_astrophysical_object = astrophysical_object(5, 'Cavity', 'gap', 0)
+
 objects_in_kep47 = [
-    astrophysical_object(0, 'Star 1', '1'),
-    astrophysical_object(1, 'Star 2', '2'),
-    astrophysical_object(2, 'kep47b', '47b'),
-    astrophysical_object(3, 'kep47c', '47d'),
-    astrophysical_object(4, 'kep47d', '47c'),
+    KepStar1_astrophysical_object,
+    KepStar2_astrophysical_object,
+    Kep47b_astrophysical_object,
+    Kep47c_astrophysical_object,
+    Kep47d_astrophysical_object,
+    cavity_astrophysical_object,
 ]
